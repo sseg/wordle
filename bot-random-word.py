20a0d95a3abc585bb4322017b4199dd49341fffa
Colin M. Saunders <colinmsaunders@gmail.com>
2021-11-27 20:22:38 -0800

    give bot-random-word it'ss own random

--- bot-random-word.py
+++ bot-random-word.py
@@ -1,16 +1,16 @@
-# sample wordle-bots bot, pick random words from word list
+# sample wordle bot, pick random words from word list
 
 
 import random
 
-random.seed('')
 
-
+g_random = None
 g_wordlists = {}
 
 
 def init():
-    global g_wordlists
+    global g_random, g_wordlists
+    g_random = random.Random('')
     for i in open('sowpods.txt').readlines():
         i = i[:-1]
         if not len(i) in g_wordlists:
@@ -20,8 +20,8 @@ def init():
 
 def play(state):
     guess_num, secret_hash, last_guess, last_score = state.split()
-    global g_wordlists
-    return random.choice(g_wordlists[len(last_score)])
+    global g_random, g_wordlists
+    return g_random.choice(g_wordlists[len(last_score)])
 
 
 init()
